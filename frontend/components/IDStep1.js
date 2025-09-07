5984cbff96588c0e8ef09bf62711b2e7d8ab1d6b
almiraramirezx-maker <almiraramirezx@gmail.com>
2025-09-07 21:59:27 +0200

    Delete frontend directory

--- frontend/components/IDStep1.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function IDStep1() {
-  return (
-    <div>IDStep1</div>
-  )
-}
-
-export default IDStep1
--- frontend/hooks/SendData.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-
-function SendData(params) {
-    axios.post(`https://ferrariback.onrender.com/create/user`, params)
-
-}
-
-export default SendData
